5dc49c0c210bec87908258c5776209089ae9011e
giannisbarbounis <giannisbarbounis@gmail.com>
2025-12-07 20:05:48 +0200

    fix

--- src/gr/aueb/cf/testgit/HelloWorld.java
+++ src/gr/aueb/cf/testgit/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World")
         System.out.println("hello coding ");
+        System.out.println("Hello coding festival");
 
     }
 
